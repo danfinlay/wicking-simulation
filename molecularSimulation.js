3d07aebda497e8649820534991058b32ed3f0b30
Dan Finlay <dan@danfinlay.com>
2023-03-25 19:09:45 -0700

    Supposedly capillary action

--- molecularSimulation.js
+++ molecularSimulation.js
@@ -1,56 +1,70 @@
-const particles = [];
-const particleCount = 200;
+let grid;
+let gridSize = 100;
+let hydrophilicChance = 0.4;
+let timeScale = 1;
+let frameCounter = 0;
 
 function setup() {
-  createCanvas(400, 400);
-  for (let i = 0; i < particleCount; i++) {
-    particles.push(new Particle());
-  }
+  const canvas = createCanvas(400, 400);
+  canvas.parent("simulation");
+  grid = new Grid(gridSize);
 }
 
 function draw() {
   background(220);
-  particles.forEach((particle) => {
-    particle.update();
-    particle.display();
-    particle.checkEdges();
-  });
+  grid.display();
+  grid.update();
 }
-
-class Particle {
-  constructor() {
-    this.position = createVector(random(width), random(height));
-    this.velocity = createVector(random(-1, 1), random(-1, 1));
-    this.acceleration = createVector(0, 0.01); // Simulating the wicking effect with upward force
-    this.size = 5;
-  }
-
-  update() {
-    this.velocity.add(p5.Vector.mult(this.acceleration, timeScale));
-    this.position.add(p5.Vector.mult(this.velocity, timeScale));
-    this.velocity.limit(3 * timeScale);
-  }
-
-  display() {
-    fill(0, 0, 255);
-    noStroke();
-    ellipse(this.position.x, this.position.y, this.size);
-  }
-
-  checkEdges() {
-    if (this.position.x > width || this.position.x < 0) {
-      this.velocity.x = -this.velocity.x;
-    }
-    if (this.position.y > height || this.position.y < 0) {
-      this.velocity.y = -this.velocity.y;
-    }
-  }
-}
-
-let timeScale = 1;
 
 function updateTimeScale() {
   const slider = document.getElementById("timeScale");
-  timeScale = parseFloat(slider.value);
+  timeScale = parseFloat(slider.value) / 100;
 }
 
+class Grid {
+  constructor(size) {
+    this.size = size;
+    this.cellSize = width / size;
+    this.cells = new Array(size * size).fill(0).map((_, i) => {
+      if (i >= size * (size - 1) && random() < hydrophilicChance) return 2; // Hydrophilic cells
+      if (i < size * (size - 1) && random() < 0.2) return 1; // Water particles
+      return 0; // Empty cells
+    });
+  }
+
+  update() {
+    frameCounter++;
+    if (frameCounter >= Math.round(1 / timeScale)) {
+      frameCounter = 0;
+      // Add the existing code for updating the cells here
+      for (let i = this.cells.length - this.size; i >= 0; i--) {
+        if (this.cells[i] === 1) {
+          let above = i - this.size;
+          let left = i - 1;
+          let right = i + 1;
+          let canMoveUp = this.cells[above] === 0 || (this.cells[above] === 2 && this.cells[left] !== 2 && this.cells[right] !== 2);
+
+          if (canMoveUp) {
+            this.cells[i] = 0;
+            this.cells[above] = 1;
+          } else if (this.cells[left] === 0) {
+            this.cells[i] = 0;
+            this.cells[left] = 1;
+          } else if (this.cells[right] === 0) {
+            this.cells[i] = 0;
+            this.cells[right] = 1;
+          }
+        }
+      }
+    }
+  }
+
+  display() {
+    for (let i = 0; i < this.cells.length; i++) {
+      let x = (i % this.size) * this.cellSize;
+      let y = floor(i / this.size) * this.cellSize;
+      fill(this.cells[i] === 1 ? "blue" : this.cells[i] === 2 ? "green" : "white");
+      rect(x, y, this.cellSize, this.cellSize);
+    }
+  }
+}
